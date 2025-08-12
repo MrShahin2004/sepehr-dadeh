e8cd8c76f81c5dfbb81dd58286ae1a5ff4825989
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-12 12:40:54 +0330

    Also forgot to commit the HomePage.vue component lmao.

---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Yo! This is the home page of Sepehr Dadeh and this component is a demonstration one.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 30px;
+}
+</style>

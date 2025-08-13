9266f8ec50d90857786256c7d7a6387af98df624
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-13 12:25:18 +0330

    Added them here.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import "./assets/style.css"
+import "./assets/font.css"
 
 const app = createApp(App);
 app.use(router);

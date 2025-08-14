358aa68de0d573186878e740392abb579bbaf58e
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-14 11:25:58 +0330

    Added the main content elements with their needed styles.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -15,8 +15,44 @@
       </div>
       <img class="w-[150px] h-[150px]" src="../assets/images/education.svg" alt="Education" style="margin-right: 2rem">
     </header>
-    <main class="w-full h-full">
-      <div class="w-[100px] h-[100px] bg-[white]"></div>
+    <h1 class="main-title text-[24px] text-[var(--main-title)] text-shadow-[0_4px_10px_black] text-center">
+      پایگاه اطلاعات قراردادهای اداره کل آموزش و پرورش استان خراسان رضوی</h1>
+    <div class="secondary-title-container w-[100%] h-[100px] flex justify-center items-center translate-y-[40px]">
+      <div class="secondary-title w-[96%] h-[70%] flex justify-end items-center rounded-xl">
+        <p class="text-[white] translate-x-[-10%]">اداره کل آموزش و پرورش خراسان رضوی</p>
+      </div>
+    </div>
+    <main class="w-full h-full flex justify-center translate-y-[35px]">
+      <div class="agreement-container w-[95%] h-[100%] flex justify-around gap-4">
+        <div class="announcement w-[25%] h-[700px] rounded-xl bg-[black]"></div>
+        <div class="agreement-list-container w-[60%] h-[700px] rounded-[6px] bg-[white] relative overflow-y-hidden">
+          <h2 style="padding: 1rem" class="text-[18px] absolute top-[0] right-[10px]">لیست قراردهای فعال</h2>
+          <div class="agreement-list w-[100%] h-[93%]
+           flex flex-col items-center justify-start overflow-y-auto translate-y-[50px]">
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+            <div class="bg-[black]"></div>
+            <div class="bg-[red]"></div>
+          </div>
+        </div>
+        <div class="agreement-search w-[25%] h-[700px] rounded-xl bg-[cyan]"></div>
+      </div>
     </main>
     <footer></footer>
   </div>
@@ -28,12 +64,41 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+:root {
+  --main-title: #dcb417;
+  --body-bg: #e5e7eb;
+  --title-bg: #48b3bd;
+  --input-bg: #dbeafe;
+  --btn-bg: #293da3;
+}
+
 .navbar {
   background-color: rgba(20, 21, 62, 0.7);
 }
 
 .other-links {
   margin-right: 1.5rem;
+}
+
+.main-title {
+  font-family: "B Titr", "Arial", sans-serif;
+}
+
+.secondary-title {
+  background-color: rgba(20, 21, 62, 0.7);
+}
+
+.agreement-list {
+  padding: 2rem;
+}
+
+.agreement-list > div {
+  width: 100%;
+  height: 120px;
+  min-height: 120px;
+  flex-shrink: 0;
+  margin-bottom: 0.5rem;
+  border-radius: 6px;
 }
 </style>

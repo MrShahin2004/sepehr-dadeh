6df139ded175287440c359b1d5a28bde357b1bc8
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-14 12:11:39 +0330

    And these two containers.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -24,7 +24,7 @@
     </div>
     <main class="w-full h-full flex justify-center translate-y-[35px]">
       <div class="agreement-container w-[95%] h-[100%] flex justify-around gap-4">
-        <div class="announcement w-[25%] h-[700px] rounded-xl bg-[black]"></div>
+        <div class="announcement w-[25%] h-[700px] rounded-xl bg-[var(--body-bg)]"></div>
         <div class="agreement-list-container w-[60%] h-[700px] rounded-[6px]
          bg-[var(--body-bg)] relative overflow-y-hidden">
           <h2 style="padding: 1rem" class="text-[18px] absolute top-[0] right-[10px]">لیست قراردهای فعال</h2>
@@ -52,7 +52,7 @@
             <div class="bg-[red]"></div>
           </div>
         </div>
-        <div class="agreement-search w-[25%] h-[700px] rounded-xl bg-[cyan]"></div>
+        <div class="agreement-search w-[25%] h-[700px] rounded-xl bg-[var(--body-bg)]"></div>
       </div>
     </main>
     <footer></footer>

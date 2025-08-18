27d630547806c6a483b5251338ab8a3bdfa326b0
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-18 11:18:14 +0330

    And set the event listener here.

--- src/components/homePageComps/HeaderElements.vue
+++ src/components/homePageComps/HeaderElements.vue
@@ -3,7 +3,7 @@
     <img class="w-[191px] h-[100px]" src="../../assets/images/khamenei.png" alt="Khamenei">
     <div class="navbar w-[50%] h-[70px] flex justify-between items-center rounded-xl translate-y-[20px]">
       <div class="w-[20%] flex items-center justify-center">
-        <a class="text-[white] hover:text-blue-500 transition" href="#">ورود به سیستم</a>
+        <a @click="RedirectToLogIn" class="text-[white] hover:text-blue-500 transition" href="#">ورود به سیستم</a>
       </div>
       <div class="other-header-links w-[80%] flex justify-end gap-5">
         <a class="text-[white] hover:text-blue-500 transition" href="#">تماس با ما</a>

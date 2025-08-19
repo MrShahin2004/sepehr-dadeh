cd51cebea466a63b5a54515afde09adcd12f4f66
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-19 11:24:00 +0330

    New line...

--- src/components/HomePageComps/AgreementItem.vue
+++ src/components/HomePageComps/AgreementItem.vue
@@ -5,7 +5,8 @@
       class="w-[100%] h-[90px] bg-[white] min-h-[90px] rounded-[6px] hover:bg-[#cbfbf1] transition relative"
   >
     <div
-        class="text-container w-[100%] h-[100%] flex flex-col justify-center items-start absolute top-[10px] right-[14px] translate-y-[-8px]"
+        class="text-container w-[100%] h-[100%]
+         flex flex-col justify-center items-start absolute top-[10px] right-[14px] translate-y-[-8px]"
     >
       <p>{{ title }}</p>
       <p class="text-center text-[13px] opacity-50">{{ date }}</p>

aab7451258ba01170c4e5c54f2447fd3a9c88a42
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-21 13:02:38 +0330

    Imported the component and handled the routing path.

--- src/router/router.js
+++ src/router/router.js
@@ -1,20 +1,22 @@
 "use strict";
 
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "@/views/HomePage.vue";
 import LogIn from "@/views/LogIn.vue";
 import ContractDetail from "@/views/ContractDetail.vue";
 import SearchPage from "@/views/SearchPage.vue";
 import TransparencyPage from "@/views/TransparencyPage.vue";
 import AdminPanel from "@/views/AdminPanel.vue";
+import NewContract from "@/views/NewContract.vue";
 
 const routes = [
-    {path: "/", component: HomePage, name: "HomePage"},
-    {path: "/login/", component: LogIn, name: "Login"},
-    {path: "/contract/:id", component: ContractDetail, name: "ContractDetail", props: true},
-    {path: "/search", component: SearchPage, name: "SearchPage", props: true},
-    {path: "/transparency", component: TransparencyPage, name: "TransparencyPage"},
-    {path: "/admin", component: AdminPanel, name: "AdminPanel", props: true},
+    { path: "/", component: HomePage, name: "HomePage" },
+    { path: "/login/", component: LogIn, name: "Login" },
+    { path: "/contract/:id", component: ContractDetail, name: "ContractDetail", props: true },
+    { path: "/search", component: SearchPage, name: "SearchPage", props: true },
+    { path: "/transparency", component: TransparencyPage, name: "TransparencyPage" },
+    { path: "/admin", component: AdminPanel, name: "AdminPanel", props: true },
+    { path: "/new-contract", component: NewContract, name: "NewContract" },
 ];
 
 const router = createRouter({

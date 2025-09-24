a552f1b86afaae3d42a99c7868e3bc87163c9407
MrShahin2004 <hashmyshahin@gmail.com>
2025-09-24 12:24:11 +0330

    Update EditContractText.vue
    Handled the redirection functions.

--- src/views/EditContractText.vue
+++ src/views/EditContractText.vue
@@ -47,17 +47,17 @@
         >
           بازگشت
         </button>
-        <button
+        <button 
           class="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 text-sm cursor-pointer"
         >
           بارگذاری سربرگ
         </button>
-        <button
+        <button @click="GoToDashboard"
           class="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 text-sm cursor-pointer"
         >
           داشبورد
         </button>
-        <button
+        <button @click="GoToCartable"
           class="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 text-sm cursor-pointer"
         >
           کارتابل

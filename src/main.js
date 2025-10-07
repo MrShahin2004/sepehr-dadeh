7b4f6834d9f75c674c02534dd9666f5de691e4cc
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-07 13:36:26 +0330

    Imported it here.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import router from './router/router'
 import "./assets/style.css"
 import "./assets/font.css"
 
+// ⬇️ add these
+import Vue3Toastify, {toast} from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+
 const app = createApp(App);
 app.use(router);
+
+// ⬇️ global config (RTL + top-right)
+app.use(Vue3Toastify, {
+    autoClose: 3000,
+    rtl: true,
+    position: toast.POSITION.TOP_RIGHT
+});
+
 app.mount('#app');

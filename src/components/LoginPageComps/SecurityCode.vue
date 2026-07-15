47ae8bcb55f71dbeb460bf776dc9725e7542361f
MrShahin2004 <hashmyshahin@gmail.com>
2026-07-15 11:11:14 +0330

    Removed the function that receives the captcha, because backend isn't available yet.

--- src/components/LoginPageComps/SecurityCode.vue
+++ src/components/LoginPageComps/SecurityCode.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="code-input bg-[#f3f4f6] w-[90%] h-fit grid grid-cols-[2.5fr_1fr_1.3fr] rounded-b-[6px_6px]">
-    <input v-model="InsertedCode" @input="SendCode"
-           style="margin: 0.5rem"
-           class="bg-[#dbeafe] border-0 rounded-[6px] text-[16px] focus:outline-0"
-           id="code-input" type="text">
+  <div
+    class="code-input bg-[#f3f4f6] w-[90%] h-fit grid grid-cols-[2.5fr_1fr_1.3fr] rounded-b-[6px_6px]"
+  >
+    <input
+      v-model="InsertedCode"
+      @input="SendCode"
+      style="margin: 0.5rem"
+      class="bg-[#dbeafe] border-0 rounded-[6px] text-[16px] focus:outline-0"
+      id="code-input"
+      type="text"
+    />
     <div class="flex justify-center items-center">
-      <img class="cursor-pointer" v-bind:src="CaptchaLink" alt="captcha" @click="GetCaptcha">
+      <img
+        class="cursor-pointer"
+        v-bind:src="CaptchaLink"
+        alt="captcha"
+        @click="GetCaptcha"
+      />
     </div>
-    <label class="flex justify-center items-center" for="code-input">کد امنیتی</label>
+    <label class="flex justify-center items-center" for="code-input"
+      >کد امنیتی</label
+    >
   </div>
 </template>
 
@@ -15,32 +28,27 @@
 export default {
   data() {
     return {
-      CaptchaLink: "",
-      InsertedCode: ""
+      InsertedCode: "",
     };
   },
   methods: {
-    GetCaptcha() {
-      // This is an obsolete code, delete or change it if not needed.
-      this.CaptchaLink = "http://localhost:4000/api/captcha/get?t" + Date.now();
-    },
     SendCode() {
       if (+this.InsertedCode.length === 0) {
         console.log("Please insert a value.");
       } else if (isNaN(+this.InsertedCode)) {
         console.log("Security code must be only a number.");
-      } else if (+this.InsertedCode.length > 5 || +this.InsertedCode.length < 5) {
+      } else if (
+        +this.InsertedCode.length > 5 ||
+        +this.InsertedCode.length < 5
+      ) {
         console.log("Security code must be exactly 5 digits.");
       } else {
         console.log("Security code meets the condition.");
         this.$emit("SendCode", +this.InsertedCode);
       }
-    }
+    },
   },
-  mounted() {
-    this.GetCaptcha();
-  }
-}
+};
 </script>
 
 <style>

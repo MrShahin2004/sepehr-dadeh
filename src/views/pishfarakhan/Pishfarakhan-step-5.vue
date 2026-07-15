49857ac30a20d6f472aef1e3a6ac538431674034
MrShahin2004 <hashmyshahin@gmail.com>
2026-07-15 13:07:55 +0330

    Edited.

--- src/views/pishfarakhan/Pishfarakhan-step-5.vue
+++ src/views/pishfarakhan/Pishfarakhan-step-5.vue
@@ -153,7 +153,7 @@
                   <input
                       type="text"
                       class="w-full rounded-md border border-gray-300 px-3 py-2"
-                      placeholder="علیرضاده"
+                      placeholder="علیزاده"
                   />
                 </div>
                 <div>
